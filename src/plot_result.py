57b35cf3d8161d96db11699cb0a9ea89c7adf8d7
JoFeG <fego.jose@gmail.com>
2023-05-18 11:26:28 -0400

    result plot

--- src/plot_result.py
+++ src/plot_result.py
@@ -2,18 +2,40 @@
 
 import argparse
 import pandas as pd
+import numpy as np
+from scipy import signal
 from matplotlib import pyplot as plt
+from .objective_function import modelo_1
 
 def main():
     args = parse_arguments()
     plot_result(args.input)
     plt.savefig(args.output)
     
-def plot_result(input_path, fig_size = (16, 7)):
-    df = pd.read_csv(input_path)
+def plot_result(df, args, x, fig_size = (16, 7)):
+    
+    model, Ts, P0, real_freq = args
+    
+    if model == 1:
+        sys = modelo_1(x, Ts)
+        
+    sim_power = P0 * np.repeat(1, real_freq.shape)
+    sim = signal.dlsim(sys, sim_power, x0 = real_freq[0])
+    sim_freq = sim[1].ravel()
+    
+    real_time = df["time"].to_numpy()
+    real_freq = df["freq"].to_numpy()
+    event_time = df["time"][df["event"]==1].to_numpy()
+    sim_time = np.arange(event_time[0],event_time[-1],Ts)
+
     fig = plt.figure(figsize=fig_size)
     
+    plt.plot(real_time,real_freq)
+    plt.axvspan(event_time.min(),event_time.max(), alpha=.1)
+    plt.plot(sim_time,sim[1])
+    
 
+    
     return fig
     
 
